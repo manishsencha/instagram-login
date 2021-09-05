c338823ecc8e60d98c5f672e94de32947a5e3df2
Manish Sencha <manishsencha2001@gmail.com>
2021-09-05 21:20:48 +0530

    UI Build

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static('client/build'))
-mongoose.connect(process.env.MONGO_URI).then(() => { console.log("Connected to database") }).catch(e => console.log(e))
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true , useUnifiedTopology: true}).then(() => { console.log("Connected to database") }).catch(e => console.log(e))
 
 app.get('/', (req, res) => {
     res.send("index.html")
@@ -19,5 +19,9 @@ app.post('/register', async (req, res) => {
     } catch {
         return res.sendStatus(404)
     }
+})
+app.get('/get', async(req, res) =>{
+    const data = await db.find();
+    return res.send(data)
 })
 app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`))
